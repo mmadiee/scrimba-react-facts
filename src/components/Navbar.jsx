2d5bb2aecc3c6b7c4456cc8c26aae85fe623bf1e
mmadiee <149312891+mmadiee@users.noreply.github.com>
2024-07-31 22:51:17 +0800

    Fixed logo image reference

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import reactLogo from '../assets/imgs/react.svg'
+import reactLogoSmall from '../assets/imgs/react-icon-small.png'
 
 export default function Navbar() {
     return (
         <nav>
             <div className="nav-logo">
-                <img src={reactLogo}/>
+                <img src={reactLogoSmall}/>
                 <h2>ReactFacts</h2>
             </div>
             <h4>React Course - Project 1</h4>
