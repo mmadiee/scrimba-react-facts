f70ffd5bbde03da0392d638b81618111a9938c77
mmadiee <149312891+mmadiee@users.noreply.github.com>
2024-07-31 22:38:04 +0800

    Added Navbar and Main Components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="nav-logo">
+                <img src="../images/react-icon-small.png" />
+                <h2>ReactFacts</h2>
+            </div>
+            <h4>React Course - Project 1</h4>
+        </nav >
+    )
+}
