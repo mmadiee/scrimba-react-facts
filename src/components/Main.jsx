a3edf5f24c6d1522524d3c13d803440a8b3c6da0
mmadiee <149312891+mmadiee@users.noreply.github.com>
2024-07-31 23:00:51 +0800

    Added styling for Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,8 @@
 export default function Main() {
     return (
         <main>
-            <h1>Fun Facts about React</h1>
-            <ul>
+            <h1 className="main--title">Fun Facts about React</h1>
+            <ul className="main--list">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 200K stars on GitHub</li>
