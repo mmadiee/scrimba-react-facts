45aba678e3659154eb7e7bb67d0ba4fd1e4132a0
mmadiee <149312891+mmadiee@users.noreply.github.com>
2024-07-31 22:43:51 +0800

    Updated logo lmage ref

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
+import reactLogo from '../assets/imgs/react.svg'
+
 export default function Navbar() {
     return (
         <nav>
             <div className="nav-logo">
-                <img src="../images/react-icon-small.png" />
+                <img src={reactLogo}/>
                 <h2>ReactFacts</h2>
             </div>
             <h4>React Course - Project 1</h4>
